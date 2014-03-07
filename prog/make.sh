f715769a092e2c6a55489994bce7d5adc6935964
Florian Richoux <florian.richoux@polytechnique.edu>
2014-03-07 17:02:36 +0100

    remove ortools

--- prog/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -fPIC -std=c++0x -O4 -DNDEBUG -I./or-tools/include -DARCH_K8 -Wno-deprecated -DUSE_CBC -DUSE_CLP -c $1.cpp -o $1.o
-
-g++ -fPIC -std=c++0x -O4 -DNDEBUG -I./or-tools/include -DARCH_K8 -Wno-deprecated -DUSE_CBC -DUSE_CLP $1.o -Wl,-rpath ./or-tools/lib -L./or-tools/lib -lortools -lz -lrt -lpthread -o $1
