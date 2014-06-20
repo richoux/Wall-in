0af5ccdc7bd2e65f3918ee8cafbdcaf23d537e09
Florian Richoux <florian.richoux@polytechnique.edu>
2014-06-20 16:24:52 +0900

    Add xp with Constraints::cost v2 (worst results on gap)

--- prog/run-100-experiments_ghost.sh
+++ prog/run-100-experiments_ghost.sh
@@ -6,8 +6,8 @@ if [ "$#" -ne 2 ]; then
 fi
 
 for i in `seq 1 100`; do
-    echo \*\*\*\*\*\*\*\*\*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_100_runs_$1_$2
-    echo \*\*\*\* $i \*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_100_runs_$1_$2
-    echo \*\*\*\*\*\*\*\*\*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_100_runs_$1_$2
-    ./run-experiments_ghost.sh $1 $2 >> scripts/ghost/xp_20th_of_June/gap_100_runs_$1_$2
+    echo \*\*\*\*\*\*\*\*\*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_with_opti_constraints_100_runs_$1_$2
+    echo \*\*\*\* $i \*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_with_opti_constraints_100_runs_$1_$2
+    echo \*\*\*\*\*\*\*\*\*\*\*\* | cat >> scripts/ghost/xp_20th_of_June/gap_with_opti_constraints_100_runs_$1_$2
+    ./run-experiments_ghost.sh $1 $2 >> scripts/ghost/xp_20th_of_June/gap_with_opti_constraints_100_runs_$1_$2
 done
