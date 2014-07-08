4fe3e5ef720e5d83c96f934a9a5738ec998fb080
Florian Richoux <florian.richoux@polytechnique.edu>
2014-07-08 18:25:19 +0900

    Remove ghost xp

--- prog/compil-experiments_ghost.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-GHOST=/home/flo/ghost
-g++ -std=c++11 -Ofast -DNDEBUG experiments/main_ghost.cpp $GHOST/src/variables/variable.cpp $GHOST/src/variables/building.cpp $GHOST/src/constraints/wallinConstraint.cpp $GHOST/src/domains/wallinDomain.cpp $GHOST/src/objectives/wallinObjective.cpp -o bin/experiments_ghost
-g++ -std=c++11 -Ofast -DNDEBUG experiments/processrawdata.cpp -o bin/processrawdata
-g++ -std=c++11 -Ofast -DNDEBUG experiments/processresults.cpp -o bin/processresults
-
